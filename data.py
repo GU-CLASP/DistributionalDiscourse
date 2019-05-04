d0593c98ec113a6fdcbae5e794ce72a9e9dfc4d0
Vladislav Maraev <vlad@maraev.me>
2019-05-04 09:58:19 +0200

    Words in integer format, vocabulary, tokenization.

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
-import torch
-import torchtext as tt
+# import torch
+# import torchtext as tt
 from swda.swda import CorpusReader
+from preproc import tokenize
 import os
 import json
 import zipfile
@@ -13,6 +14,8 @@ args = parser.parse_args()
 
 SWDA_CORPUS_DIR = "data/swda"
 SWDA_SPLITS = "data/swda_{}.json"
+
+vocab =  {"@@@@@":0, "spkr_A":1, "spkr_B":2}
 
 def gen_splits(id_list, train=0.7, val=0.1, test=0.2):
     assert(train+val+test == 1)
@@ -42,21 +45,37 @@ def prep_swda():
     else: # save the splits file
         splits = gen_splits(list(corpus.keys()))
         with open(splits_file, 'w') as f:
-            json.dump(splits, f) 
+            json.dump(splits, f)
+
+    def words_to_ints(ws):
+        maxvalue = max(vocab.values())
+        for w in ws:
+            if w not in vocab:
+                maxvalue += 1
+                vocab[w] = maxvalue
+        xs = [vocab[x] for x in ws]
+        return xs
 
     def extract_example(transcript):
         """ Gets the parts we need from the SWDA utterance object """ 
-        tags, utts = [], []
+        tags, utts, utts_ints = [], [], []
         for utt in transcript.utterances:
-            utts.append(utt.text.lower())
+            words = "spkr_{} ".format(utt.caller) + tokenize(utt.text.lower())
+            ints = words_to_ints(words.split())
+            utts.append(words)
+            utts_ints.append(ints)
             tags.append(utt.act_tag)
-        return {'id': transcript.conversation_no, 'utts': utts, 'tags': tags}
+        return {'id': transcript.conversation_no, 'utts': utts, 'utts_ints': utts_ints, 'tags': tags}
 
     splits = {split: [extract_example(corpus[ex_id]) for ex_id in splits[split]] for split in splits}
     for split in splits:
         with open(SWDA_SPLITS.format(split), 'w') as f:
             json.dump(splits[split], f)
+    with open(SWDA_SPLITS.format("vocab"), 'w') as f:
+        json.dump(vocab, f)
 
 if __name__ == '__main__':
     if args.step == 'prep-swda':
         prep_swda()
+
+        
