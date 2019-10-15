04c9f68bd7e6b05c5e0050b0997678f1e0ba8f98
Bill Noble <winobes@gmail.com>
2019-10-15 16:03:57 +0200

    fix shell.nix for pytorch_pretrained_bert 0.62

--- shell.nix
+++ shell.nix
@@ -1,33 +1,50 @@
-{ nixpkgs ? import <nixpkgs> {} }:
-let
-   nixpkgs_source = fetchTarball https://github.com/NixOS/nixpkgs/archive/19.03.tar.gz;
-   # Older configuration:
-   # nixpkgs_source = nixpkgs.fetchFromGitHub { # for safety of checking the hash
-   #   owner = "jyp";
-   #   repo = "nixpkgs";
-   #   rev = "cudnn7.3-cuda9.0";
-   #   sha256 = "1jvsagry3842slgzxkqmp66mxs5b3clbpm7xijv1vjp2wxxvispf";
-   # };
-   
-in
-with (import nixpkgs_source {}).pkgs;
+{ bootstrap ? import <nixpkgs> {} }:
 
-let py = (python36.buildEnv.override {
-  extraLibs =  with python36Packages; 
-    [nltk
-     pytorch
-     notebook
-     tqdm
-     # you can add other packages here. (comment out as necessary)
-     # scikitimage
-     # gensim
-     # scikitlearn
+let
+    # pkgs_source = fetchTarball "https://github.com/NixOS/nixpkgs/archive/dfd8f84aef129f1978e446b5d45ef05cd4421821.tar.gz";
+    # pkgs_source = ~/repo/nixpkgs;
+    pkgs_source = bootstrap.fetchFromGitHub { # for safety of checking the hash
+        owner = "stites";
+        repo = "nixpkgs";
+        rev = "pytorch_11";
+        sha256 = "1w1bdqnpjcgdmql3jfcmyz0g0wix4xg17417a7ask5bsphbhpia2";
+      };
+    overlays = [
+      (self: super:  # define our local packages
+         {
+          python3 = super.python3.override {
+           packageOverrides = python-self: python-super: {
+             pytorch-pretrained-bert = python-self.callPackage /opt/nix/pytorch-pretrained-bert-0.6.2.nix { };
+           };};})
+      (import /opt/nix/nvidia-410.78.nix)  # fix version of nvidia drivers
+      (self: super: {
+          cudatoolkit = super.cudatoolkit_10; # fix version of cuda
+          cudnn = super.cudnn_cudatoolkit_10;})
     ];
-  ignoreCollisions = true;});
-in pkgs.stdenv.mkDerivation {
-    name = "python-env";
-    buildInputs = [ py ];
+    config = {
+      allowUnfree = true;
+      cudaSupport = true;
+    };
+    pkgs = import pkgs_source {inherit overlays; inherit config;};
+    py = pkgs.python3;
+    pyEnv = py.buildEnv.override {
+      extraLibs = with py.pkgs;
+        [
+         # If you want to have a local virtualenv, see here: https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/python.section.md
+         pytorch-pretrained-bert
+         pytorch
+	 nltk
+         # scikitlearn
+         # notebook
+         # etc
+        ];
+      ignoreCollisions = true;};
+in
+  pkgs.stdenv.mkDerivation {
+    name = "sh-env";
+    buildInputs = [pyEnv];
     shellHook = ''
+      export LANG=en_US.UTF-8
       export PYTHONIOENCODING=UTF-8
-      '';
-     }
+     '';
+  }
